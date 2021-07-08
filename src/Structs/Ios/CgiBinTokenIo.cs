8a14f3b73c3436890e8bd557056e9f6eab2629e0
Cinling <1105412681@qq.com>
2021-07-08 17:42:51 +0800

    Add CanDictionaryClassAttribute

--- src/Structs/Ios/CgiBinTokenIo.cs
+++ src/Structs/Ios/CgiBinTokenIo.cs
@@ -1,4 +1,5 @@
 ﻿using Cinling.Lib.Attributes;
+using Cinling.Lib.Enums;
 using Cinling.Lib.Structs.Ios;
 
 namespace LibWechat.Structs.Ios {
@@ -6,6 +7,7 @@ namespace LibWechat.Structs.Ios {
     /// <summary>
     /// 
     /// </summary>
+    [CanDictionaryClass(CanDictionary.Underscore)]
     public class CgiBinTokenIo : BaseIo {
         public string GrantType { get; set; }
         public string Appid { get; set; }
